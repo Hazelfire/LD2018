d9f659749b375e43acb6897d39e0ff42dba39629
silentcoder99 <silentcoder99@gmail.com>
2018-12-01 14:18:15 +1100

    Add boilerplate code

---
+++ main.lua
@@ -0,0 +1,14 @@
+wf = require 'windfield'
+
+function love.load()
+    world = wf.newWorld(0, 0, true)
+    world:setGravity(0, 512)
+end
+
+function love.update()
+
+end
+
+function love.draw()
+
+end
