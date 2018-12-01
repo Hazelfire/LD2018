2f420559c8455bea4beb1aa7a894988e035ac947
silentcoder99 <silentcoder99@gmail.com>
2018-12-01 20:00:32 +1100

    Add platforms

--- terrain.lua
+++ terrain.lua
@@ -5,6 +5,22 @@ terrain = P
 
 local WALL_WIDTH = 32
 
+function P:makeLevel(world, sprites)
+    P:makeBoundary(world, sprites['platform_support.png'])
+
+    P:makePlatform(5, 16, 3, 1, world, sprites['platform.png'])
+    P:makePlatform(10, 15, 5, 1, world, sprites['platform.png'])
+end
+
+function P:makePlatform(tx, ty, tw, th, world, sprite)
+    local x = tx * sprite:getWidth()
+    local y = ty * sprite:getHeight()
+    local w = tw * sprite:getWidth()
+    local h = th * sprite:getHeight()
+
+    Wall:new(x, y, w, h, world, sprite)
+end
+
 function P:makeBoundary(world, sprite)
     local width, height = love.graphics.getDimensions()
     
