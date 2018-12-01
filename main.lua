7a098bb06cbfd3150a86a272c28c013a62a6c4a6
silentcoder99 <silentcoder99@gmail.com>
2018-12-01 18:45:47 +1100

    Add wall sprites to fix

--- main.lua
+++ main.lua
@@ -1,18 +1,22 @@
 wf = require 'windfield'
+SpriteLoader = require 'spriteLoader'
 Manager = require 'manager'
 Terrain = require 'terrain'
 Wall = require 'wall'
 
 function love.load()
-    love.window.setMode(800, 600)
+    love.window.setMode(800, 608)
     
+    sprites = SpriteLoader:loadSprites()
+
     world = wf.newWorld(0, 0, true)
     world:setGravity(0, 512)
+    world:addCollisionClass('ground')
 
     manager = Manager:new()
     world.manager = manager
 
-    Terrain:makeBoundary(world)
+    Terrain:makeBoundary(world, sprites['platform_support.png'])
 end
 
 function love.update(dt)
--- wall.lua
+++ wall.lua
@@ -3,16 +3,19 @@ wall = P
 
 local prototype = {}
 
-function P:new(x, y, w, h, world)
+function P:new(x, y, w, h, world, sprite)
     local object = {}
 
     object.x = x
     object.y = y
     object.w = w
     object.h = h
+    object.world = world
+    object.sprite = sprite
 
     object.collider = world:newRectangleCollider(x, y, w, h)
     object.collider:setType('static')
+    object.collider:setCollisionClass('ground')
 
     setmetatable(object, prototype)
     prototype.__index = prototype
@@ -24,8 +27,9 @@ end
 
 function prototype:render()
     love.graphics.push()
-        love.graphics.setColor(1, 1, 1)
-        love.graphics.rectangle('fill', self.x, self.y, self.w, self.h)
+        self.sprite:setWrap('repeat', 'repeat')
+        quad = love.graphics.newQuad(0, 0, self.w, self.h, self.sprite:getWidth(), self.sprite:getHeight())
+        love.graphics.draw(self.sprite, quad, self.x, self.y)
     love.graphics.pop()
 end
 
