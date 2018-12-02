3ff0187e06cf543cc584b3df90cdc51e5167ccf6
silentcoder99 <silentcoder99@gmail.com>
2018-12-02 11:38:52 +1100

    Add 3d background and thin platforms

--- terrain.lua
+++ terrain.lua
@@ -1,4 +1,5 @@
 Wall = require 'wall'
+Background = require 'background'
 
 local P = {}
 terrain = P
@@ -7,12 +8,13 @@ local WALL_WIDTH = 32
 
 function P:makeLevel(world, sprites)
     P:makeBoundary(world, sprites)
+    P:makeBackground(world, sprites)
 
-    P:makePlatform(5, 16, 3, 1, world, sprites['platform.png'])
-    P:makePlatform(10, 15, 5, 1, world, sprites['platform.png'])
+    P:makePlatform(5, 16, 3, world, sprites['platform.png'])
+    P:makePlatform(10, 15, 5, world, sprites['platform.png'])
 end
 
-function P:makePlatform(tx, ty, tw, th, world, sprite)
+function P:makeWall(tx, ty, tw, th, world, sprite)
     local x = tx * sprite:getWidth()
     local y = ty * sprite:getHeight()
     local w = tw * sprite:getWidth()
@@ -21,14 +23,40 @@ function P:makePlatform(tx, ty, tw, th, world, sprite)
     Wall:new(x, y, w, h, world, sprite)
 end
 
+function P:makePlatform(tx, ty, tw, world, sprite)
+    local x = tx * sprite:getWidth()
+    local y = ty * sprite:getHeight()
+    local w = tw * sprite:getWidth()
+
+    Wall:new(x, y, w, 5, world, sprite)
+end
+
+function P:makeBackgroundWall(tx, ty, tw, th, world, sprite)
+    local x = tx * sprite:getWidth()
+    local y = ty * sprite:getHeight()
+    local w = tw * sprite:getWidth()
+    local h = th * sprite:getHeight()
+
+    Background:new(x, y, w, h, world, sprite)
+end
+
 function P:makeBoundary(world, sprites)
     local width, height = love.graphics.getDimensions()
 
-    P:makePlatform(0, 0, 1, 18, world, sprites['left wall.png'])
-    P:makePlatform(0, 18, 1, 1, world, sprites['floor left corner.png'])
-    P:makePlatform(1, 18, 23, 1, world, sprites['floor.png'])
-    P:makePlatform(24, 18, 1, 1, world, sprites['floor right corner.png'])
-    P:makePlatform(24, 0, 1, 18, world, sprites['right wall.png'])
+    P:makeWall(0, 0, 1, 18, world, sprites['left wall.png'])
+    P:makeWall(0, 18, 1, 1, world, sprites['floor left corner.png'])
+    P:makeWall(1, 18, 23, 1, world, sprites['floor.png'])
+    P:makeWall(24, 18, 1, 1, world, sprites['floor right corner.png'])
+    P:makeWall(24, 0, 1, 18, world, sprites['right wall.png'])
+end
+
+function P:makeBackground(world, sprites)
+    love.graphics.setBackgroundColor(135/255, 135/255, 135/255)
+    P:makeBackgroundWall(1, 0, 1, 17, world, sprites['next to left wall.png'])
+    P:makeBackgroundWall(1, 17, 1, 1, world, sprites['background floor left corner.png'])
+    P:makeBackgroundWall(2, 17, 21, 1, world, sprites['above floor.png'])
+    P:makeBackgroundWall(23, 17, 1, 1, world, sprites['right corner.png'])
+    P:makeBackgroundWall(23, 0, 1, 17, world, sprites['next to right wall.png'])
 end
 
 return terrain
